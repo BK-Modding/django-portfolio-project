7c1b544c630e9b7281680d0060ce69d8c2aa35f0
BK-Modding <kavesbteja@gmail.com>
2018-05-29 23:07:02 +0530

    Jobs complete

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pubdate = models.DateTimeField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
