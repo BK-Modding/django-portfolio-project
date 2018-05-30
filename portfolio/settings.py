4fdde864bc6a64796b7bde45b9f4dd1c6c03ff40
BK-Modding <kavesbteja@gmail.com>
2018-05-31 01:58:56 +0530

    to github

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'Hypermodder1',
+        'PASSWORD': 'kappa',
         'HOST': 'localhost',
         'PORT': '5432'
     }
